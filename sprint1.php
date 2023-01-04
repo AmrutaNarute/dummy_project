6782629f0a8ed5393ffcdbb8b461c331fe3da236
AmrutaNarute <amruta.narute@dwebpixel.com>
2023-01-04 22:27:14 +0530

    Some minor changes

--- sprint1.php
+++ sprint1.php
@@ -2,4 +2,26 @@
    echo "hello Narute";
    echo "hello Amruta";
    echo "hello world";
+   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";
+   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";   echo "hello Narute";
+   echo "hello Amruta";
+   echo "hello world";
 ?>
